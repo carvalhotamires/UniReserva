983c4097cef3a85319a87c4074b00dccea9c5c5f
carvalhotamires <tamiresoak@gmail.com>
2026-04-29 20:11:20 -0300

    feat: criar projeto UniRoom - sistema de reserva de salas

---
+++ src/main/java/com/uniesp/unireserva/UnireservaApplication.java
@@ -0,0 +1,13 @@
+package com.uniesp.unireserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnireservaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnireservaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniesp/unireserva/UnireservaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniesp.unireserva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnireservaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
