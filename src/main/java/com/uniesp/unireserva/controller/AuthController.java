f03cc3b1feaf765acb31f81c84da3ace69bbdffa
carvalhotamires <tamiresoak@gmail.com>
2026-05-10 16:29:21 -0300

    Ajusta controllers após revisão

--- src/main/java/com/uniesp/unireserva/controller/AuthController.java
+++ src/main/java/com/uniesp/unireserva/controller/AuthController.java
@@ -1,4 +1,32 @@
 package com.uniesp.unireserva.controller;
 
+import com.uniesp.unireserva.dto.request.LoginRequestDTO;
+import com.uniesp.unireserva.dto.response.LoginResponseDTO;
+import com.uniesp.unireserva.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    @PostMapping("/login")
+    public LoginResponseDTO login(@RequestBody LoginRequestDTO dto) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        dto.getEmail(),
+                        dto.getPassword()
+                )
+        );
+
+        String token = jwtService.generateToken(dto.getEmail());
+
+        return new LoginResponseDTO(token);
+    }
 }
--- src/main/java/com/uniesp/unireserva/controller/RoomController.java
+++ src/main/java/com/uniesp/unireserva/controller/RoomController.java
@@ -1,4 +1,32 @@
 package com.uniesp.unireserva.controller;
 
+import com.uniesp.unireserva.dto.request.RoomRequestDTO;
+import com.uniesp.unireserva.dto.response.RoomResponseDTO;
+import com.uniesp.unireserva.service.interfaces.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rooms")
+@RequiredArgsConstructor
 public class RoomController {
+
+    private final RoomService roomService;
+
+    @PostMapping
+    public RoomResponseDTO create(@RequestBody RoomRequestDTO dto) {
+        return roomService.create(dto);
+    }
+
+    @GetMapping
+    public List<RoomResponseDTO> findAll() {
+        return roomService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public RoomResponseDTO findById(@PathVariable Long id) {
+        return roomService.findById(id);
+    }
 }
