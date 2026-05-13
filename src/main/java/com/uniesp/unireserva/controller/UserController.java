571a8048dc19df06e2330c5aa520c1614e91620b
carvalhotamires <tamiresoak@gmail.com>
2026-05-13 19:14:45 -0300

    fix: resolve conflito no UserController

--- src/main/java/com/uniesp/unireserva/controller/UserController.java
+++ src/main/java/com/uniesp/unireserva/controller/UserController.java
@@ -1,4 +1,33 @@
 package com.uniesp.unireserva.controller;
 
-public class Usercontroller {
+import com.uniesp.unireserva.dto.request.UserRequestDTO;
+import com.uniesp.unireserva.dto.response.UserResponseDTO;
+import com.uniesp.unireserva.service.interfaces.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public UserResponseDTO create(@RequestBody UserRequestDTO dto) {
+        return userService.create(dto);
+    }
+
+    @GetMapping
+    public List<UserResponseDTO> findAll() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public UserResponseDTO findById(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
 }
