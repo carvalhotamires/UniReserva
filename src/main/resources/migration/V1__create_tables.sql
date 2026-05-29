a889c57c237785c7b42df5876d879b3d768774bf
carvalhotamires <tamiresoak@gmail.com>
2026-05-29 18:18:40 -0300

    Corrigindo erros no projeto

--- src/main/resources/migration/V1__create_tables.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE reservations (
-                              id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                              reservation_date DATE,
-                              start_time TIME,
-                              end_time TIME,
-                              status VARCHAR(50),
-                              user_id BIGINT,
-                              room_id BIGINT
-);
