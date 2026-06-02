61000704b1f4e630c8a77c32d8799b6c5c96b56f
Messias <messiasguedesdev@gmail.com>
2026-06-01 22:46:03 -0300

    ADMIN pode tudo, os outros só nas suas contas. Falta fazer isso na reservation

--- src/main/java/com/uniesp/unireserva/controller/ReservationController.java
+++ src/main/java/com/uniesp/unireserva/controller/ReservationController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -40,6 +41,12 @@ public class ReservationController {
     public ResponseEntity<ReservationResponseDTO> findById(@PathVariable Long id) {
         ReservationResponseDTO reservation = reservationService.findById(id);
         return ResponseEntity.ok(reservation);
+    }
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public List<ReservationResponseDTO> findAll() {
+        return reservationService.findAll();
     }
 
     // Excluir/cancelar reserva
